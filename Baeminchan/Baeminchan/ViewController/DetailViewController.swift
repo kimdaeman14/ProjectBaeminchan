f2607a8688004d142962fc2d316370234d6da79f
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-14 02:22:44 +0900

    프로젝트 생성

---
+++ Baeminchan/Baeminchan/ViewController/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  Baeminchan
+//
+//  Created by kimdaeman14 on 2018. 8. 14..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\n---------- [ dsf ] ----------\n")
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
